9ccf30b24fe5aad0b57b5a1cc9fd49af0709247e
adi <tiliztiadi@gmail.com>
2026-01-28 22:03:01 +0700

    fix: revisi 27012026

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -10,36 +10,36 @@
         <div class="row justify-content-center text-center mb-5">
 
             <div class="col-md-3 col-6 mb-3">
-                <a href="{{ url('surat-masuk') }}" class="btn btn-outline-dark w-100 py-4">
-                    <div class="h1 fw-bold">{{ $suratMasukCount }}</div>
+                <a href="{{ url('surat-masuk') }}" class="btn gradient-1 w-100 py-4">
+                    <div class="h1 font-weight-bold text-white">{{ $suratMasukCount }}</div>
                     <div class="text-uppercase">Surat Masuk</div>
                 </a>
             </div>
 
             <div class="col-md-3 col-6 mb-3">
-                <a href="{{ url('surat-keluar') }}" class="btn btn-outline-dark w-100 py-4">
-                    <div class="h1 fw-bold">{{ $suratKeluarCount }}</div>
+                <a href="{{ url('surat-keluar') }}" class="btn gradient-2  w-100 py-4">
+                    <div class="h1 font-weight-bold text-white">{{ $suratKeluarCount }}</div>
                     <div class="text-uppercase">Surat Keluar</div>
                 </a>
             </div>
 
             <div class="col-md-3 col-6 mb-3">
-                <a href="{{ url('surat-masuk') }}" class="btn btn-outline-dark w-100 py-4">
-                    <div class="h1 fw-bold">{{ $userCount }}</div>
+                <a href="{{ url('surat-masuk') }}" class="btn gradient-3 w-100 py-4">
+                    <div class="h1 font-weight-bold text-white">{{ $userCount }}</div>
                     <div class="text-uppercase">User</div>
                 </a>
             </div>
 
             <div class="col-md-3 col-6 mb-3">
-                <a href="{{ url('laporan') }}" class="btn btn-outline-dark w-100 py-4">
-                    <div class="text-uppercase h1 fw-bold" style="padding: 10px 0px">Laporan</div>
+                <a href="{{ url('laporan') }}" class="btn gradient-4 w-100 py-4">
+                    <div class="text-uppercase h1 font-weight-bold text-white" style="padding: 10px 0px">Laporan</div>
                 </a>
             </div>
         </div>
 
         <div class="row">
             <div class="col-12 text-center my-5">
-                <h1 class="fw-bold text-uppercase">
+                <h1 class="font-weight-bold text-uppercase">
                     Sistem Pengarsipan<br>
                     Surat Menyurat
                 </h1>
@@ -47,13 +47,18 @@
         </div>
 
 
-        <div class="row">
-            <div class="col-12 text-center mt-5">
-                <h5 class="text-uppercase h2 fw-bold">
-                    Selamat Datang {{ auth()->user()->nama }}
-                </h5>
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card shadow-lg" style="background-color:#ca3e00; border-radius:12px;">
+                    <div class="card-body text-center py-4">
+                        <h3 class="text-white font-weight-bold text-uppercase mb-0">
+                            Selamat Datang {{ auth()->user()->nama }}
+                        </h3>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 @endsection
 
